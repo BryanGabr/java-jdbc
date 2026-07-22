6d9d0d911f5593c051c014ab30c0227b508ecc2a
Bryan Gabriel <gabrielbryan717@gmail.com>
2026-07-22 20:30:50 -0300

    First commit, learing Java JDBC

---
+++ src/main/java/br/com/bryan/jdbc/ConnectionFactoryTest/ConnectionFactoryTest.java
@@ -0,0 +1,9 @@
+package br.com.bryan.jdbc.ConnectionFactoryTest;
+
+import br.com.bryan.jdbc.conn.ConnectionFactory;
+
+public class ConnectionFactoryTest {
+    public static void main(String[] args) {
+        ConnectionFactory.getConnection();
+    }
+}
